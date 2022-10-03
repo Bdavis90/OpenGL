62ba104485edfe02e7af46b780d4c786f35d3028
brandon.davis <brandon.davis@darkmatter2bd.com>
2022-10-03 16:56:40 -0400

    Window and input

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,50 +1,125 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <sstream>
 
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Window 1";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+bool gFullScreen = false;
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
 int main(void)
 {
-	GLFWwindow* window;
+	GLFWwindow* pWindow = NULL;
 
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
+	// Set min OpenGL Version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Force to create modern core profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (gFullScreen)
+	{
+		GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
+		if (pVmode) 
+		{
+			pWindow = glfwCreateWindow(pVmode->width, pVmode->height, APP_TITLE, pMonitor, NULL);
+		}
+	}
+	else
+	{
+		/* Create a windowed mode window and its OpenGL context */
+		pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+
+	}
+
+	if (!pWindow)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
 	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(pWindow);
 
+	glfwSetKeyCallback(pWindow, glfw_onKey);
+
+	glewExperimental = GL_TRUE;
+	// Initialize glew after window context
 	if (glewInit() != GLEW_OK)
+	{
 		std::cout << "Error!" << std::endl;
+		return -1;
+	}
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(pWindow))
 	{
+		showFPS(pWindow);
 		/* Render here */
+		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+		//glBegin(GL_TRIANGLES);
+		//glVertex2f(-0.5f, -0.5f);
+		//glVertex2f(0.0f, 0.5f);
+		//glVertex2f(0.5f, -0.5f);
+		//glEnd();
 
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
+		/* Swap front and back buffers. Front is being displayed, back is currently being drawn */
+		glfwSwapBuffers(pWindow);
 
-		/* Poll for and process events */
+		/* Poll for and process kb/m events */
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as a double
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// limit text update 4 times per second
+
+	if (elapsedSeconds > .25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		std::ostringstream outs;
+		outs.precision(3);
+		outs << std::fixed << APP_TITLE << "   " << "FPS: " << fps
+			<< "Frame Time: " << msPerFrame << " (ms)";
+
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		frameCount = 0;
+	}
+
+	frameCount++;
 }
