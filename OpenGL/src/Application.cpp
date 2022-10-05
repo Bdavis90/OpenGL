f99f39d607e71c1a4f3712632ada60131201ff3c
brandon.davis <brandon.davis@darkmatter2bd.com>
2022-10-05 16:56:14 -0400

    --create shader -- created different buffer layouts

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,6 +12,30 @@ GLFWwindow* gWindow = NULL;
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
 void showFPS(GLFWwindow* window);
 bool initOpenGL();
+bool gWireframe = false;
+
+const GLchar* vertexShaderSrc =
+// what shader model to use
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;"
+"layout (location = 1) in vec3 color;"
+"out vec3 vert_color;"
+"void main()"
+"{"
+"	vert_color = color;"
+"	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+"}";
+
+const GLchar* fragmentShaderSrc =
+"#version 330 core\n"
+"in vec3 vert_color;"
+"out vec4 frag_color;"
+"void main()"
+"{"
+"	frag_color = vec4(vert_color, 1.0f);"
+"}";
+
+
 int main(void)
 {
 	if (!initOpenGL())
@@ -20,10 +44,16 @@ int main(void)
 		return -1;
 	}
 
+
+#pragma region Interleaved Buffer Layout
+
+#if 0
+
 	GLfloat vertices[] = {
-		0.0f,  0.5f, 0.0f, // Top
-		0.5f, -0.5f, 0.0f, // Right
-		-0.5f, -0.5f, 0.0f // Left
+		// position			// color
+		0.0f,  0.5f, 0.0f,	1.0f, 0.0f, 0.0f,// Top
+		0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// Right
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f // Left
 	};
 
 	// Vertex buffer object
@@ -35,16 +65,182 @@ int main(void)
 	glGenBuffers(1, &vbo);
 	// Makes buffer the current one. Only one at a time.
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Fill buffer with data from vertices. Static Draw: The data store contents will be modified once and used many times.
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+	// Fill buffer with data from vert_pos. Static Draw: The data store contents will be modified once and used many times.
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
+
+	// INTERVEAVED BUFFER EXAMPLE
+	// position
 	//stride: amount of bytes between each vertex. pointer: amount of bytes between attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 3, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, 0);
 	glEnableVertexAttribArray(0);
 
-	const GLchar* vertexShaderSrc = "#version 330 core\n" "layout (location = 0) in vec3 pos;";
+	// color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (GLvoid*)(sizeof(GLfloat) * 3));
+	glEnableVertexAttribArray(1);
+#endif // 0
+
+#pragma endregion 
+
+#pragma region Separate Buffer Layout
+#if 0
+	GLfloat vert_pos[] = {
+		0.0f,  0.5f, 0.0f, // Top
+		0.5f, -0.5f, 0.0f, // Right
+		-0.5f, -0.5f, 0.0f, // Left
+	};
+
+	GLfloat vert_color[] = {
+		1.0f, 0.0f, 0.0f, // Top
+		0.0f, 1.0f, 0.0f, // Right
+		0.0f, 0.0f, 1.0f  // Left
+	};
+
+	// Vertex buffer object
+	GLuint vbo, vbo2;
+
+	// Vertex array object
+	GLuint vao;
+	// Create memory in the graphics card
+	glGenBuffers(1, &vbo);
+
+	// POSITION
+	// Makes buffer the current one. Only one at a time.
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// Fill buffer with data from vert_pos. Static Draw: The data store contents will be modified once and used many times.
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), vert_pos, GL_STATIC_DRAW);
+
+	// COLOR
+	// Create memory in the graphics card
+	glGenBuffers(1, &vbo2);
+	// Makes buffer the current one. Only one at a time.
+	glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+	// Fill buffer with data from vert_pos. Static Draw: The data store contents will be modified once and used many times.
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), vert_color, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// POSITION
+	//stride: amount of bytes between each vertex. pointer: amount of bytes between attribute
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	// COLOR
+	glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+#endif // 0
+
+#pragma endregion
+
+#pragma region Quad
+#if 1
+	const GLchar* vertexShaderSrc =
+		// what shader model to use
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 pos;"
+		"out vec3 vert_color;"
+		"void main()"
+		"{"
+		"	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+		"}";
+
+	const GLchar* fragmentShaderSrc =
+		"#version 330 core\n"
+		"out vec4 frag_color;"
+		"void main()"
+		"{"
+		"	frag_color = vec4(.35f, .96f, .3f, 1.0f);"
+		"}";
+
+	GLfloat vert_pos[] = {
+	// tri 0
+	-0.5f, 0.5f, 0.0f,
+	0.5f, 0.5f, 0.0f,
+	0.5f,  -0.5f, 0.0f,
+
+	// tri 1
+	-0.5f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f,  -0.5f, 0.0f,
+	};
+
+	// Vertex buffer object
+	GLuint vbo, vbo2;
+
+	// Vertex array object
+	GLuint vao;
+	// Create memory in the graphics card
+	glGenBuffers(1, &vbo);
+	// Makes buffer the current one. Only one at a time.
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// Fill buffer with data from vert_pos. Static Draw: The data store contents will be modified once and used many times.
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(GLfloat), vert_pos, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
+	//stride: amount of bytes between each vertex. pointer: amount of bytes between attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+#endif // 0
+
+#pragma endregion
+
+
+	//Create vertex shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	// use our vertex shader source code
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	// compile our vertex shader
+	glCompileShader(vs);
+
+	GLint result;
+	GLchar infoLog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! Vertex shader failed to compile. " << infoLog << std::endl;
+	}
+
+	//Create fragment shader
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	// use our fragment shader source code
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	// compile our fragment shader
+	glCompileShader(fs);
+
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fs, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! Fragment shader failed to compile. " << infoLog << std::endl;
+	}
+
+	// Create program for our shader
+	GLuint shaderProgram = glCreateProgram();
+	// Attach our shaders to the program
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(infoLog), NULL, infoLog);
+		std::cerr << "Error! Shader Program linker failed. " << infoLog << std::endl;
+	}
+
+	// Delete already linked shaders
+	glDeleteShader(vs);
+	glDeleteShader(fs);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(gWindow))
@@ -54,8 +250,10 @@ int main(void)
 		glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shaderProgram);
+
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
 
 
@@ -65,6 +263,12 @@ int main(void)
 		/* Poll for and process kb/m events */
 		glfwPollEvents();
 	}
+
+
+	// Delete the program before exiting
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
 
 	glfwTerminate();
 	return 0;
@@ -129,6 +333,19 @@ void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key == GLFW_KEY_W && action == GLFW_PRESS)
+	{
+		gWireframe = !gWireframe;
+		if (gWireframe)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		else
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
 	}
 }
 
